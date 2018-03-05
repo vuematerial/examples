3f4980d65b866218684504f979cd8c84125c8987
Samuell <samuell.patro@gmail.com>
2018-03-05 17:36:29 +0100

    Add quick-start example

---
+++ examples/quick-start/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <md-toolbar class="md-primary">
+        <h1 class="md-title">Vue Material</h1>
+    </md-toolbar>
+    
+    <md-content>
+        Your content here
+    </md-content>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss">
+.md-content {
+  padding: 16px;
+}
+</style>
